7f575edae4d95b719c4d697c4cdea5e4fae7587e
Office Impresso <wagnerra@gmail.com>
2025-06-12 14:06:49 -0300

    atualização 6.7

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -8,15 +8,6 @@ use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 class Kernel extends ConsoleKernel
 {
     /**
-     * The Artisan commands provided by your application.
-     *
-     * @var array
-     */
-    protected $commands = [
-        //
-    ];
-
-    /**
      * Define the application's command schedule.
      *
      * @param  \Illuminate\Console\Scheduling\Schedule  $schedule
@@ -29,14 +20,20 @@ class Kernel extends ConsoleKernel
 
         if ($env === 'live') {
             //Scheduling backup, specify the time when the backup will get cleaned & time when it will run.
-            $schedule->command('backup:run')->dailyAt('23:50');
+            
+            $schedule->command('backup:clean')->daily()->at('01:00');
+            $schedule->command('backup:run')->daily()->at('01:30');
+
 
             //Schedule to create recurring invoices
             $schedule->command('pos:generateSubscriptionInvoices')->dailyAt('23:30');
             $schedule->command('pos:updateRewardPoints')->dailyAt('23:45');
+
+            $schedule->command('pos:autoSendPaymentReminder')->dailyAt('8:00');
+
         }
 
-        if ($env === 'demo' && !empty($email)) {
+        if ($env === 'demo') {
             //IMPORTANT NOTE: This command will delete all business details and create dummy business, run only in demo server.
             $schedule->command('pos:dummyBusiness')
                     ->cron('0 */3 * * *')
